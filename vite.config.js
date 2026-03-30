4eb1fdccb9c660a2468b50e53263be06a887c1fd
Ryan W. Moore <30422172+rwm6857@users.noreply.github.com>
2026-03-30 11:20:39 -0400

    chore: scaffold root config files (index.html, package.json, vite, env, wrangler)

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  base: '/',
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: 'generateSW',
+      manifest: false, // we supply our own manifest.webmanifest in /public
+      workbox: {
+        // App shell: cache-first
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
+        // Stem WAV files: cache on first play, then serve from cache
+        runtimeCaching: [
+          {
+            urlPattern: /\.wav$/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'stems-cache',
+              expiration: {
+                maxEntries: 200,
+                maxAgeSeconds: 60 * 60 * 24 * 90, // 90 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: false,
+      },
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1200,
+  },
+})
